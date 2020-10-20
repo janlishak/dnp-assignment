e75d772f27e765377809c607921fc0bb2598a295
Lenka <leorincakova@gmail.com>
2020-10-20 17:59:42 +0200

    Add project files.

---
+++ Ass1/Data/Impl/InMemoryUserService.cs
@@ -0,0 +1,46 @@
+﻿using Ass1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ass1.Data.Impl
+{
+    public class InMemoryUserService : IUserService
+    {
+        private List<User> users;
+
+        public InMemoryUserService()
+        {
+            users = new[]
+                {
+                new User
+                {
+                    UserName = "Lenka",
+                    Password="password",
+                    SecurityLevel = 3
+                },
+                new User
+                {
+                    UserName = "Lucia",
+                    Password="pass",
+                    SecurityLevel= 1
+                }
+            }.ToList();
+        }
+        public User ValidateUser(string userName, string password)
+        {
+            User first = users.FirstOrDefault(user => user.UserName.Equals(userName));
+            if(first==null)
+            {
+                throw new Exception("User not found");
+
+            }
+            if(!first.Password.Equals(password))
+            {
+                throw new Exception("Incorrect password");
+            }
+            return first;
+        }
+    }
+}
---
+++ Ass1/Models/Job.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ass1.Models
+{
+     enum Job
+    {
+        Teacher,
+        Engineer,
+        GarbageCollector, 
+        Shepherd,
+        Pilot,
+        PoliceOfficer,
+        Pirate,
+        Fireman,
+        Astronaut,
+        Captain,
+        Solider,
+        PizzaChef,
+        Chef,
+        Janitor,
+        FactoryWorker,
+        Chauffeur,
+        Ninja,
+        Doctor,
+        Nurse,
+        Chemist,
+        Caretaker,
+        Gardener,
+        Mascot,
+        Athlete,
+        Other
+
+    }
+}
